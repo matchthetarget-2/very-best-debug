85f1a1d99444ca05b6c53529c6611dad910d7a99
Ben Purinton <ben.purinton@gmail.com>
2023-05-31 10:10:09 -0800

    rebuild

--- config/initializers/appdev_support.rb
+++ config/initializers/appdev_support.rb
@@ -1,5 +1,6 @@
 AppdevSupport.config do |config|
-  config.action_dispatch = false;
-  config.active_record   = false;
+  config.action_dispatch = true;
+  config.active_record   = true;
+  config.pryrc           = :full;
 end
 AppdevSupport.init
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,15 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  get("/", { :controller => "users", :action => "index" })
+  get("/users", { :controller => "users", :action => "index" })
+  get("/users/:username", { :controller => "users", :action => "show" })
+  get("/insert_user_record", { :controller => "users", :action => "create" })
+  get("/update_user/:user_id", { :controller => "users", :action => "update" })
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get("/venues", { :controller => "venues", :action => "index" })
+  get("/insert_venue_record", { :controller => "venues", :action => "create" })
+  get("/venues/:venue_id", { :controller => "venues", :action => "show" })
+  get("/update_venue/:venue_id", { :controller => "venues", :action => "update" })
+  get("/delete_venue/:venue_id", { :controller => "venues", :action => "destroy" })
+
+  get("/insert_comment_record", { :controller => "comments", :action => "create" })
 end
